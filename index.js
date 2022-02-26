099144cba0633da22dbf56dd2c7857fffbe815e3
Alexander Balasch <balaschalexander@gmail.com>
2022-02-26 13:04:43 +0100

    first basic mockup

---
+++ index.js
@@ -0,0 +1,46 @@
+var languages = {
+    en: {
+        main_title: "Which help do you need?",
+        emergency: "Emergency",
+        non_Emergency: "non Emergency",
+        emergeny_title: "What kind of emergency do you have?",
+        non_emergeny_title: "What kind of help do you need?",
+        medical: "Medical",
+        fire: "Fire",
+        food_and_water: "Food and Water",
+        shelter: "Shelter",
+        mental_health: "Mental health"
+    },
+
+    de: {
+        main_title: "Welche Hilfe benötigen sie?",
+        emergency: "Notfall",
+        non_Emergency: "kein Notfall",
+        emergeny_title: "Welchen Notfall haben sie?",
+        non_emergeny_title: "Welche Hilfe benötigen sie?",
+        medical: "Medizinisch",
+        fire: "Feuer",
+        food_and_water: "Essen und Wasser",
+        shelter: "Unterkunft",
+        mental_health: "Psychologisch"
+    }
+}
+
+function get_selected_language() {
+    return document.getElementById("langselector").value
+}
+
+function loadlang() {
+    lang = languages[get_selected_language()];
+    //console.log(lang)
+    for (var element in lang) {
+        try {
+            document.getElementById(element).innerHTML = lang[element];
+        } catch (error) {
+            //console.error(error);
+            //console.error("error ocurred for element: " + element);
+        }
+    };
+}
+
+loadlang()
